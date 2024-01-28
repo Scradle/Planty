b4cb59868dc7e667f2d27720190e6fdf6b2d4def
Scradle <152749477+Scradle@users.noreply.github.com>
2024-01-28 22:08:34 +0100

    ajout de multicans et formulaire de contact

--- app/public/wp-content/themes/blankslate-child/functions.php
+++ app/public/wp-content/themes/blankslate-child/functions.php
@@ -6,3 +6,27 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/theme.css', array(), filemtime(get_stylesheet_directory() . '/theme.css'));
 }
 
+function ajouter_lien_admin_menu($items, $args) {
+    // Vérifier si l'utilisateur est connecté et si l'emplacement du menu est 'main-menu'
+    if (is_user_logged_in() && $args->theme_location == 'main-menu') {
+        // Récupérer le lien vers l'administration WordPress
+        $admin_url = admin_url();
+
+        // Créer le lien pour l'administration
+        $lien_administration = '<li class="menu-item menu-item-administration"><a href="' . esc_url($admin_url) . '">Admin</a></li>';
+
+        // Diviser les éléments du menu en un tableau
+        $menu_items = explode('</li>', $items);
+
+        // Insérer le lien d'administration en deuxième position
+        array_splice($menu_items, 1, 0, $lien_administration);
+
+        // Recoller les éléments du menu en une chaîne
+        $items = implode('</li>', $menu_items);
+    }
+
+    return $items;
+}
+
+// Ajouter le filtre pour modifier les éléments du menu
+add_filter('wp_nav_menu_items', 'ajouter_lien_admin_menu', 10, 2);
